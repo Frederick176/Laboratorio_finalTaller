9afddcffb5fe8ec0dfa977e4a14d813193fdf683
Fredy Hernández <fhernandez-2023176@kinal.edu.gt>
2025-03-05 03:57:00 -0600

    build: Se realizo la documentacion

--- src/auth/auth.routes.js
+++ src/auth/auth.routes.js
@@ -1,11 +1,66 @@
 import { Router } from "express";
 import { register, login } from "./auth.controller.js";
-import { registerValidator, loginValidator} from "../middlewares/user-validator.js";
+import { registerValidator, loginValidator } from "../middlewares/user-validator.js";
 
-const router = Router()
+const router = Router();
 
-router.post("/register", registerValidator, register)   
+/**
+ * @swagger
+ * /register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: johndoe
+ *               password:
+ *                 type: string
+ *                 example: Password123!
+ *               email:
+ *                 type: string
+ *                 example: johndoe@example.com
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Bad request
+ */
+router.post("/register", registerValidator, register);
 
-router.post("/login", loginValidator, login)
+/**
+ * @swagger
+ * /login:
+ *   post:
+ *     summary: Login a user
+ *     tags: [Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: johndoe
+ *               password:
+ *                 type: string
+ *                 example: Password123!
+ *     responses:
+ *       200:
+ *         description: User logged in successfully
+ *       400:
+ *         description: Bad request
+ *       401:
+ *         description: Unauthorized
+ */
+router.post("/login", loginValidator, login);
 
-export default router
+export default router;
--- src/user/user.routes.js
+++ src/user/user.routes.js
@@ -3,12 +3,91 @@ import { updateUser, getUser, deleteUser } from "./user.controller.js";
 import { validateJWT } from "../middlewares/validate-jwt.js";
 import { updateProfileValidator } from "../middlewares/user-validator.js";
 
-const router = Router()
+const router = Router();
 
-router.put("/:uid", validateJWT, updateProfileValidator, updateUser)
+/**
+ * @swagger
+ * /user/{uid}:
+ *   put:
+ *     summary: Update a user
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: uid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: User ID
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               name:
+ *                 type: string
+ *                 example: John
+ *               surname:
+ *                 type: string
+ *                 example: Doe
+ *               email:
+ *                 type: string
+ *                 example: johndoe@example.com
+ *               phone:
+ *                 type: string
+ *                 example: 12345678
+ *     responses:
+ *       200:
+ *         description: User updated successfully
+ *       400:
+ *         description: Bad request
+ *       403:
+ *         description: Forbidden
+ *       500:
+ *         description: Internal server error
+ */
+router.put("/:uid", validateJWT, updateProfileValidator, updateUser);
 
-router.get("/", validateJWT, getUser)
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: Get all users
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: Users retrieved successfully
+ *       500:
+ *         description: Internal server error
+ */
+router.get("/", validateJWT, getUser);
 
-router.delete("/delete/:uid", validateJWT, deleteUser)
+/**
+ * @swagger
+ * /user/delete/{uid}:
+ *   delete:
+ *     summary: Delete a user
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: uid
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: User ID
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ *       401:
+ *         description: Unauthorized
+ *       403:
+ *         description: Forbidden
+ *       404:
+ *         description: User not found
+ *       500:
+ *         description: Internal server error
+ */
+router.delete("/delete/:uid", validateJWT, deleteUser);
 
-export default router
+export default router;
